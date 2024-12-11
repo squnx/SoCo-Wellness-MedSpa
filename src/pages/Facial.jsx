3acca64aed3be756b68c63a3d5326285e3bb2342
Ivan Song <mousewurx@gmail.com>
2024-12-11 15:53:28 -0800

    hide tab images on mobile device

--- src/pages/Facial.jsx
+++ src/pages/Facial.jsx
@@ -38,7 +38,7 @@ const Facial = () => {
                       <h4>Botox</h4>
                       <p className="fst-italic">Botox is a widely popular, non-surgical treatment designed to smooth out fine lines and wrinkles by temporarily relaxing the muscles that cause them. Perfect for areas such as the forehead, between the brows, and around the eyes, Botox offers a quick and effective solution for maintaining a youthful, refreshed appearance. The procedure is minimally invasive, with little to no downtime, allowing you to return to your daily routine immediately after treatment. Our skilled professionals will work with you to achieve natural-looking results, enhancing your features without looking overdone.</p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center order-1 order-lg-2 d-none d-sm-block">
                       <img src="/assets/images/services/facial-1.jpg" alt="" className="img-fluid" />
                     </div>
                   </div>
@@ -49,7 +49,7 @@ const Facial = () => {
                       <h4>Filler</h4>
                       <p className="fst-italic">Facial fillers are an excellent solution for restoring volume and fullness to areas of the face that may have lost elasticity or plumpness over time. Whether you're looking to add volume to the cheeks, smooth out smile lines, or enhance your lips, dermal fillers provide immediate results. Our range of fillers, including hyaluronic acid-based products, are safe and effective in rejuvenating the skin and giving your face a more youthful, contoured appearance. The procedure is quick, with minimal discomfort, and results can last anywhere from several months to over a year, depending on the treatment area.</p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center order-1 order-lg-2 d-none d-sm-block">
                       <img src="/assets/images/services/facial-2.jpg" alt="" className="img-fluid" />
                     </div>
                   </div>
@@ -60,7 +60,7 @@ const Facial = () => {
                       <h4>Sculptra</h4>
                       <p className="fst-italic">Sculptra is an injectable treatment that works beneath the skin to stimulate natural collagen production, helping to restore lost volume and smooth out wrinkles. Unlike traditional fillers, Sculptra provides gradual, natural-looking results over time as collagen builds in areas such as the cheeks, temples, and jawline. It's an ideal solution for addressing deeper wrinkles and volume loss. The results can last up to two years, offering long-term rejuvenation with minimal downtime, leaving you with a refreshed and youthful appearance.</p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center order-1 order-lg-2 d-none d-sm-block">
                       <img src="/assets/images/services/facial-3.jpg" alt="" className="img-fluid" />
                     </div>
                   </div>
@@ -71,7 +71,7 @@ const Facial = () => {
                       <h4>Thread</h4>
                       <p className="fst-italic">Thread lifting is an innovative non-surgical treatment designed to lift and tighten sagging skin, creating a more youthful and rejuvenated appearance. This procedure involves the insertion of specialized threads into the skin, which gently lift and stimulate collagen production for a firmer, more lifted look. It's ideal for areas such as the jawline, cheeks, and neck, providing an instant lifting effect with long-lasting benefits. The procedure is minimally invasive, with little to no downtime, and results improve over time as your skin regenerates and tightens naturally.</p>
                     </div>
-                    <div className="col-lg-4 text-center order-1 order-lg-2">
+                    <div className="col-lg-4 text-center order-1 order-lg-2 d-none d-sm-block">
                       <img src="/assets/images/services/facial-3.jpg" alt="" className="img-fluid" />
                     </div>
                   </div>
@@ -153,7 +153,7 @@ const Facial = () => {
         </div>
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-10" data-aos="fade-up" data-aos-delay="100">
+            <div className="col-lg-8" data-aos="fade-up" data-aos-delay="100">
               {/* <div className="faq-container">
                 {facialData.map((faq, index) => (
                   <div key={index} className="faq-item">
