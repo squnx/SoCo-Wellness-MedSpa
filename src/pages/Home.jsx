4039a203887e33c0e6094d4cf07fa76dbf6d253e
Ivan Song <mousewurx@gmail.com>
2024-12-19 18:17:06 -0800

    purchaed Adobe Stock subscription and updated with 18 licenced photos on Services page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       bgStyle: 'transparent-60',
     },
     {
-      background: '/assets/images/home/hero-03.jpg',
+      background: '/assets/images/home/hero-promos.jpg',
       title: 'Promos',
       text: 'Exclusive Promotions & Savings at SoCo',
       bgStyle: 'transparent-60',
